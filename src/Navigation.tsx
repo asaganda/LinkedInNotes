968f120b875a18d76820c2993de19615b7f7623b
Andrew <Asaganda@gmail.com>
2026-02-26 22:12:06 -0500

    add shadcn button component for use in navbar, modify a bit

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,12 +1,16 @@
+import { Button } from "./components/ui/button"
+
 const Navigation = () => {
     return (
         <header>
             <nav>
                 <div className="nav-title">
-                    <a href="#">LinkedIn Connections</a>
+                    {/* <a href="#">LinkedIn Connections</a> */}
+                    <span className="text-white">LinkedIn Connections</span>
                 </div>
                 <div className="nav-contact">
-                    <button>+ new contact</button>
+                    {/* <button>+ new contact</button> */}
+                    <Button variant="outline" size="lg">+ New Contact</Button>
                 </div>
             </nav>
         </header>
